47f61837c486bf7b96c43378d66d00a4ffb3c62f
Michael Makukha <m.makukha@gmail.com>
2019-10-14 16:57:36 +0300

    Initial commit

---
+++ tasks.py
@@ -0,0 +1,66 @@
+import io
+from invoke import task
+import json
+import os
+from pathlib import Path
+import re
+import sys
+
+PTY = (os.name != 'nt')
+DOCKER_RUN = f'docker run {"-it" if PTY else ""} --rm'
+
+
+HOMEDIR = Path('.').resolve()
+BUILDDIR = HOMEDIR / 'build'
+DATADIR  = HOMEDIR / 'data'
+
+BUILDDIR.mkdir(parents=True, exist_ok=True)
+
+
+@task
+def build(c):
+    """Build docker image."""
+    c.run(f'docker-compose build', pty=PTY)
+
+
+@task
+def run(c, task):
+    """Run python script."""
+
+    # determine task type and run python script
+    tasks = {
+        'file': (
+            re.compile(r'(?P<filename>[a-zA-Z][a-zA-Z0-9_]*\.py)'),
+            f'python {{filename}}'),
+        'function': (
+            re.compile(r'(?P<module>[a-zA-Z][a-zA-Z0-9_]*):(?P<function>[a-zA-Z][a-zA-Z0-9_]*)(?P<args>\(.*\))'),
+            f'python -c \'import {{module}}; {{module}}.{{function}}{{args}}\''),
+        }
+    cmdline = None
+    for name, (rx, cmd) in tasks.items():
+        m = rx.fullmatch(task)
+        if m is not None:
+            cmdline = cmd.format(**m.groupdict())
+            break
+    if cmdline is not None:
+        c.run(f'docker-compose run master {cmdline}', replace_env=False, pty=PTY)
+    else:
+        raise ValueError(f'Unsupported task definition: {task}')
+
+
+@task
+def shell(c):
+    """Open shell in docker container."""
+    c.run(f'docker-compose run master /bin/bash', pty=PTY)
+
+
+@task()
+def pyspark(c):
+    """Run PySpark in client container."""
+    c.run(f'docker-compose run --rm --no-deps client pyspark', pty=PTY)
+
+
+@task
+def submit(c, cmd):
+    """Run Spark command."""
+    c.run(f'docker-compose run --rm client spark-submit {cmd}', pty=PTY)
---
+++ yahoo.py
@@ -0,0 +1,155 @@
+import aiofiles
+from aiohttp import ClientSession
+import asyncio
+from collections import defaultdict
+import csv
+import io
+import lxml.html
+from pathlib import Path
+import pyarrow as pa
+import pyarrow.parquet as pq
+import sys
+import tarfile
+from tqdm import tqdm
+
+from tasks import BUILDDIR, DATADIR
+
+YAHOO_ARCH = BUILDDIR / 'yahoo.tbz2'
+YAHOO_DATA = BUILDDIR / 'yahoo.csv'
+YAHOO_HTMLS = BUILDDIR / 'yahoo_html'
+YAHOO_PARQUET = BUILDDIR / 'yahoo.parquet'
+
+
+NASDAQ_FILES = (
+    DATADIR / 'nasdaq' / 'amex.csv',
+    DATADIR / 'nasdaq' / 'nasdaq.csv',
+    DATADIR / 'nasdaq' / 'nyse.csv',
+    )
+
+
+def read_symbols():
+    """Read symbols from NASDAQ dataset"""
+
+    symbols = set()
+
+    for filename in NASDAQ_FILES:
+        with open(filename) as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                symbols.add(row['Symbol'].upper().strip())
+
+    return list(sorted(symbols))
+
+
+def scrape_descriptions_async():
+    """Scrape companies descriptions asynchronously."""
+
+    symbols = read_symbols()
+    progress = tqdm(total=len(symbols), file=sys.stdout, disable=False)
+    YAHOO_HTMLS.mkdir(parents=True, exist_ok=True)
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Safari/605.1.15',
+        }
+
+    async def fetch(symbol, session):
+        async with session.get(f'https://finance.yahoo.com/quote/{symbol}/profile') as response:
+            text = await response.read()
+            async with aiofiles.open(YAHOO_HTMLS / f'{symbol}.html', 'wb') as f:
+                await f.write(text)
+            progress.update(1)
+
+    async def run(symbols):
+        async with ClientSession(headers=headers) as session:
+            tasks = (asyncio.ensure_future(fetch(symbol, session)) for symbol in symbols)
+            await asyncio.gather(*tasks)
+
+    loop = asyncio.get_event_loop()
+    loop.set_exception_handler(lambda x, y: None)  # suppress exceptions because of bug in Python 3.7.3 + aiohttp + asyncio
+    loop.run_until_complete(asyncio.ensure_future(run(symbols)))
+    progress.close()
+
+
+def compress_descriptions(encoding='utf-8', batch_size=1000, compression='BROTLI'):
+    """Convert tarfile to parquet"""
+
+    names = ('symbol', 'html')
+
+    def read_incremental():
+        """Incremental generator of batches"""
+        with tarfile.open(YAHOO_ARCH) as archive:
+            batch = defaultdict(list)
+            for member in tqdm(archive):
+                if member.isfile() and member.name.endswith('.html'):
+                    batch['symbol'].append(Path(member.name).stem)
+                    batch['html'].append(archive.extractfile(member).read().decode(encoding))
+                    if len(batch['symbol']) >= batch_size:
+                        yield pa.Table.from_arrays([pa.array(batch[n]) for n in names], names)
+                        batch = defaultdict(list)
+            if batch:
+                yield pa.Table.from_arrays([pa.array(batch[n]) for n in names], names)  # last partial batch
+
+    writer = None
+    for batch in read_incremental():
+        if writer is None:
+            writer = pq.ParquetWriter(YAHOO_PARQUET, batch.schema, use_dictionary=False, compression=compression, flavor={'spark'})
+        writer.write_table(batch)
+    writer.close()
+
+
+def decompress_descriptions(encoding='utf-8'):
+    """Convert parquet to tarfile"""
+
+    pf = pq.ParquetFile(YAHOO_PARQUET)
+
+    progress = tqdm(file=sys.stdout, disable=False)
+
+    with tarfile.open(YAHOO_ARCH, 'w:bz2') as archive:
+        for i in range(pf.metadata.num_row_groups):
+            table = pf.read_row_group(i)
+            columns = table.to_pydict()
+            for symbol, html in zip(columns['symbol'], columns['html']):
+                bytes = html.encode(encoding)
+                s = io.BytesIO(bytes)
+                tarinfo = tarfile.TarInfo(name=f'yahoo/{symbol}.html')
+                tarinfo.size = len(bytes)
+                archive.addfile(tarinfo=tarinfo, fileobj=s)
+                progress.update(1)
+
+    progress.close()
+
+
+def parse_descriptions(src=YAHOO_PARQUET, dst=YAHOO_DATA):
+    """Parse scraped pages."""
+
+    reader = pq.ParquetFile(src)
+
+    with tqdm(total=reader.metadata.num_rows) as progress:
+
+        with open(dst, 'w', encoding='utf-8') as f:
+            writer = csv.DictWriter(f, fieldnames=['symbol', 'sector', 'industry', 'employees', 'description'])
+            writer.writeheader()
+
+            for g in range(reader.metadata.num_row_groups):
+                table = reader.read_row_group(g).to_pydict()
+                for symbol, html in zip(table['symbol'], table['html']):
+                    tree = lxml.html.fromstring(html)
+
+                    row = {'symbol': symbol.strip()}
+                    row['description'] = '\n'.join(tree.xpath('//section[h2//*[text()="Description"]]/p/text()'))
+                    info = (tree.xpath('//div[@class="asset-profile-container"]//p[span[text()="Sector"]]') or [None])[0]
+                    if info is not None:
+                        row['sector'] = (info.xpath('./span[text()="Sector"]/following-sibling::span[1]/text()') or [''])[0]
+                        row['industry'] = (info.xpath('./span[text()="Industry"]/following-sibling::span[1]/text()') or [''])[0]
+                        row['employees'] = (info.xpath('./span[text()="Full Time Employees"]/following-sibling::span[1]/span/text()') or [''])[0].replace(',', '')
+
+                    writer.writerow(row)
+                    progress.update()
+
+
+def main():
+    scrape_descriptions_async()
+
+
+if __name__ == '__main__':
+    main()
